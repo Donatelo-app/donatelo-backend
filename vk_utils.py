bbc2ef4857ffc4e6af1b919d4e06fb347146a055
Alexander Mamaev <alxmamaev@ya.ru>
2017-11-14 23:41:10 +0500

    Update vk utils

--- vk_utils.py
+++ vk_utils.py
@@ -1,5 +1,6 @@
 import jinja2
 import requests
+from io import BytesIO
 
 get_upload_url = jinja2.Template("https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?group_id={{group_id}}&crop_x=0&crop_y=0&crop_x2=1590&crop_y2=400&access_token={{access_token}}&v=5.64")
 accept_url = jinja2.Template("https://api.vk.com/method/photos.saveOwnerCoverPhoto?hash={{phash}}&photo={{photo}}&access_token={{access_token}}&v=5.65")
